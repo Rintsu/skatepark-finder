0ffd0079c8cf647eb6f6e16ec4e93e2a0d7128a6
Kuukso Riina <s1800242@student.businesscollege.fi>
2018-11-27 12:26:53 +0200

    First few locations for testing the functionalities

---
+++ src/locationOptions.js
@@ -0,0 +1,83 @@
+var skateparks =
+	[
+		{
+			"name":"Aalto University",
+			"address":"Ossinlampi, 02150 Espoo",
+			"type":"Miniramp",
+			"material":"Plywood",
+			"lightning":"No",
+			"entranceFees":"Free",
+			"images":"./images/JussiRossi-foto-KekeLeppala.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		},
+		{
+			"name":"Eira",
+			"address":"Merisatamanranta, 00150 Helsinki",
+			"type":"Street",
+			"material":"Concrete, tarmac",
+			"lightning":"Yes",
+			"entranceFees":"Free",
+			"images":"./images/Ponkes-Skatepark-1.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		},
+		{
+			"name":"Eläintarha",
+			"address":"Nordenskiöldinkatu 20, 00250 Helsinki",
+			"type":"Street, pool",
+			"material":"Concrete, tile",
+			"lightning":"Yes",
+			"entranceFees":"Free",
+			"images":"./images/micropolis-970x970.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		},
+		{
+			"name":"Olari",
+			"address":"Ylismäentie 15, 02200 Espoo",
+			"type":"Street, pool",
+			"material":"Concrete",
+			"lightning":"Yes",
+			"entranceFees":"Free",
+			"images":"./images/olari-1.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		},
+		{
+			"name":"Espoonlahti",
+			"address":"Espoonlahdentie 2-4, 02360 Espoo",
+			"type":"Street",
+			"material":"Tarmac, concrete elements",
+			"lightning":"No",
+			"entranceFees":"Free",
+			"images":"./images/espoonlahti-1.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		},
+		{
+			"name":"Kirkkojärvi",
+			"address":"Kylävainionkuja, 02760 Espoo",
+			"type":"Street",
+			"material":"Concrete, tarmac",
+			"lightning":"Yes",
+			"entranceFees":"Free",
+			"images":"./images/kirkkojarvi-1.jpg",
+			"comments":{
+				"username":"",
+				"comment":""
+			}
+		}
+	];
+
+export default skateparks;
