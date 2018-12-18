68b7de7bb5efbd402122e1980cdeca23eb7960b1
Kuukso Riina <s1800242@student.businesscollege.fi>
2018-12-18 14:23:49 +0200

    Fetching data from database, instead of local file.

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 import LocationInfo from './StatelessLocation';
-import locationOptions from './locationOptions.js';
-
-//Mapping all the dropdown menu options
-const options = locationOptions.map( location => ({ label: location.name, value: location.name }));
+import './index.css';
 
 let skateParkElement;
 
@@ -12,10 +9,25 @@ class Dropdown extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			selectedOption: null
+			selectedOption: null,
+			skateparks: []
 		};
 		//Binding is not needed if the method syntax is: handleMethod = () => {}
 		// this.handleChange = this.handleChange.bind(this);
+	}
+
+	//executes before rendering
+	componentWillMount(){
+		this.getParks();
+	}
+
+	getParks = () => {
+		fetch('http://localhost:5000/parks')
+		.then(response => response.json())
+		.then(response => this.setState({
+			skateparks: response.data
+		}))
+		.catch(err => console.error(err))
 	}
 
 	handleChange = (selectedPark) => {
@@ -24,26 +36,29 @@ class Dropdown extends Component {
 		});
 		console.log(`Option selected: `, selectedPark.value);
 
-		//Cannot use fetch() since the API is local
-		//Checking which data to import from locationOptions.js, according to the selected dropdown option
-		let locationData = checkOption();
-		newOption(locationData);
-
-			function checkOption(){
-				for (let i = 0; i < locationOptions.length; i++){
-					if(selectedPark.value === locationOptions[i].name){
-						return locationOptions[i]
-					}
-				}
+		//Checking which data to import from database based on user selection
+		let locationData;
+		let parks = this.state.skateparks;
+		for (let i = 0; i < parks.length; i++){
+			if(selectedPark.value === parks[i].skatepark_id){
+				locationData = parks[i]
 			}
+		}
+		skateParkElement = <LocationInfo locationOptions = {locationData} />;
+	}
 
-			function newOption(locationData){
-					//Populating the LocationInfo component with the right location data
-					skateParkElement = <LocationInfo locationOptions = {locationData} />;
-			}
+	//assigning the dropdown options
+	renderParks = () => {
+		let options = [];
+		let parks = this.state.skateparks;
+		for (let i = 0; i < parks.length; i++){
+				options.push({value: parks[i].skatepark_id, label: parks[i].name});
+		}
+		return options;
 	}
 
 	render(){
+		const options = this.renderParks();
 		return(
 			<div>
 				<div className="dropdown">
